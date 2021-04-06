da2e77fbf795e25126499691dfb9fefa7c75f986
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-06 21:21:46 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,11 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  
+
 end
 
 def average_student_gpa
-  
+
 end
 
 def total_tardies_for_all_students
@@ -17,5 +17,5 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
-  
+
 end
